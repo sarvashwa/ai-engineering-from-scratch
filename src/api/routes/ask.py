6fd889fdd42606253839272668cebb8d3691d7dc
Sarvashwa Singh <sarvashwa@gmail.com>
2026-07-20 18:12:44 +0530

    Added new api endpoint to get streaming response

--- src/api/routes/ask.py
+++ src/api/routes/ask.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, status, Depends, BackgroundTasks
+from fastapi.responses import StreamingResponse
 
 from src.api.background_log import background_log
 from src.api.dependencies import get_rag_service
@@ -12,14 +13,14 @@ router = APIRouter(
 )
 
 @router.post(
-        "",
-        response_model=AskResponse,
-        summary="Ask a question",
-        status_code=status.HTTP_200_OK,
-        description=(
-        "Accepts a user question, forwards it to the RAG service, "
-        "and returns an AI-generated answer."
-        ),
+    "",
+    response_model=AskResponse,
+    summary="Ask a question",
+    status_code=status.HTTP_200_OK,
+    description=(
+    "Accepts a user question, forwards it to the RAG service, "
+    "and returns an AI-generated answer."
+    ),
 )
 def ask(
     request: AskRequest,
@@ -37,9 +38,33 @@ def ask(
 @router.post("/background-test")
 def background_test(
     background_tasks: BackgroundTasks,
-):
+    ):
     background_tasks.add_task(background_log)
 
     return {
         "message": "Response returned immediately."
     }
+
+@router.post(
+        "/stream",
+        summary="Ask a question",
+        status_code=status.HTTP_200_OK, 
+        description=(
+        "Accepts a user question, forwards it to the RAG service, "
+        "and returns an AI-generated answer like chatgpt."
+        ),
+        )
+def ask_stream(
+    request: AskRequest,
+    rag_service: RAGService = Depends(get_rag_service)
+    ):
+    
+    stream = rag_service.stream_answer(
+        question = request.question,
+        top_k = request.top_k
+    )
+
+    return StreamingResponse(
+        stream,
+        media_type = "text/plain"
+    )
