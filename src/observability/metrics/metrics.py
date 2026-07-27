206302331caa17e223762c23a79438df186aacac
Sarvashwa Singh <sarvashwa@gmail.com>
2026-07-27 21:01:50 +0530

    export request metrics using OpenTelemetry console exporter

--- src/observability/metrics/metrics.py
+++ src/observability/metrics/metrics.py
@@ -1,5 +1,10 @@
 from opentelemetry import metrics
 from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import InMemoryMetricReader
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
 
 class Metrics:
 
@@ -8,10 +13,26 @@ class Metrics:
 
 def create_metrics() -> Metrics:
 
-    meter_provider = MeterProvider()
+    # metric_reader = InMemoryMetricReader()
+    
+    # meter_provider = MeterProvider(
+    #     metric_readers=[metric_reader]
+    # )
+
+    exporter = ConsoleMetricExporter()
+
+    metric_reader = PeriodicExportingMetricReader(
+        exporter=exporter
+    )
+
+    meter_provider = MeterProvider(
+        metric_readers=[metric_reader]
+    )
     metrics.set_meter_provider(meter_provider)
 
     meter = metrics.get_meter("ai-engineering-from-scratch")
+
+    metrics.set_meter_provider(meter_provider)
 
     request_counter = meter.create_counter(
         name = "rag.requests",
