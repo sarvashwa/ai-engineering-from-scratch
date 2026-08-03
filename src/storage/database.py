02f003384d9bbfcf8b8a90f7887ea7361be778e2
Sarvashwa Singh <sarvashwa@gmail.com>
2026-08-03 13:27:20 +0530

    create first SQLAlchemy model and generate database schema

--- src/storage/database.py
+++ src/storage/database.py
@@ -1,6 +1,20 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from src.config.config import load_settings
+from src.storage.models.base import Base
+from src.storage.models.document import Documents
 
 settings = load_settings()
-print(settings.DATABASE_URL)
+
 engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+Base.metadata.create_all(engine)
+
+print("Tables created successfully!")
---
+++ src/storage/models/document.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.storage.models.base import Base
+
+class Documents(Base):
+    __tablename__ = "documents"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
