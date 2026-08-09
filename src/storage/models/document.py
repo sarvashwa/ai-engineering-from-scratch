d24a995ba5968d6690f31ec0347f7d8fce6dd04e
Sarvashwa Singh <sarvashwa@gmail.com>
2026-08-09 13:54:04 +0530

    Introduce users table and document foreign key

--- src/storage/models/document.py
+++ src/storage/models/document.py
@@ -1,3 +1,4 @@
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.storage.models.base import Base
@@ -10,3 +11,8 @@ class Document(Base):
     title: Mapped[str] = mapped_column(nullable=False)
 
     summary: Mapped[str | None] = mapped_column(nullable=True)
+
+    user_id: Mapped[int | None] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=True
+    )
