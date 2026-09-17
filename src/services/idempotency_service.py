577cef4fa68e2e9fdbfd8fb16d6ba5fa2c88d76c
Sarvashwa Singh <sarvashwa@gmail.com>
2026-09-17 15:49:03 +0530

    add idempotency service

--- src/services/idempotency_service.py
+++ src/services/idempotency_service.py
@@ -13,3 +13,6 @@ class IdempotencyService:
 
     def get_idempotency_key(self, key: str, user_id: int) -> IdempotencyKey:
         return self._idempotency_repository.get_by_key_and_user(key, user_id)
+
+    def create(self, idempotency_key: IdempotencyKey):
+        self._idempotency_repository.create(idempotency_key)
